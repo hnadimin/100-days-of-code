3e43039ea49140eb2c64018b27f4a2aa2b2255d5
hnadiminops <hnadiminops@gmail.com>
2023-04-06 21:23:55 +0100

    added go program-2

---
+++ LearnGolang/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("golang" + "program")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("9.0/2.0 =", 9.0/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
